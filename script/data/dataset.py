a45ba4a440bf384a9875f44ceb8d571d04a9c3d2
chobe111 <chobe1@naver.com>
2021-06-09 11:18:47 +0900

    first commit

---
+++ script/data/dataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+import glob
+import os
+
+
+class DicomImageDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __len__():
+        pass
