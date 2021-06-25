5fb5576d6c1be3d4131ea061d0495f0a3331990c
chobe111 <chobe1@naver.com>
2021-06-25 16:50:20 +0900

    Add Data TransformerClass

---
+++ src/data/transformer.py
@@ -0,0 +1,22 @@
+import torchvision.transforms as transforms
+import torch.nn as nn
+
+
+class TransformerA:
+    def __init__(self) -> None:
+        pass
+
+
+class TransformerB:
+    def __init__(self) -> None:
+        pass
+
+
+class TransformerC:
+    def __init__(self) -> None:
+        pass
+
+
+class TransformerD:
+    def __init__(self) -> None:
+        pass
